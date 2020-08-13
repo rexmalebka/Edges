2e50fa95ba76f5a86957566813d0c33ff62d98ff
Dorian Velazquez <dorian.velazquez@digitalonus.com>
2020-08-12 19:33:27 -0500

    finished walls

--- underborders/js/Server.js
+++ underborders/js/Server.js
@@ -6,6 +6,7 @@ export const Server = {
 		let nickname = "anon-"+Math.random().toString(16).slice(2,6)
 		const texture = "avTex1.jpg"
 		let pos = {x:-550.8884125720521, y: 14, z: -490.75654644504897}
+//		let pos = {x:-17.4, y: 14, z: -63.75654644504897}
 		let rot = {x:0,y:-Math.PI,z:0}
 		//{x: 549.164734959957, y: 13.999999999999964, z: 512.1886469707863}		
 		if(!localStorage.getItem("uuid")){
@@ -39,8 +40,7 @@ export const Server = {
 			}else{
 				pos = {x: 549.164734959957, y: 13.999999999999964, z: 512.1886469707863}		
 				//--
-				pos = {x: 418, y: 14, z: 626}		
-				
+				pos = {x: -402.80011575306787, y: 14.000000000000052, z: -507.0356680010567}
 				rot = {x:0, y:-Math.PI/2, z:0}
 			}
                 }
